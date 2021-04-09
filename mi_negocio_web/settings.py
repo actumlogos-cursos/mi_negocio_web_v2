9c977509f6459d270116d9983ca68ec99dba7522
Jesus <jmvazqueznicolas@gmail.com>
2021-04-09 02:41:26 -0500

    Ordenes listo

--- mi_negocio_web/settings.py
+++ mi_negocio_web/settings.py
@@ -41,6 +41,12 @@ INSTALLED_APPS = [
     'nucleo',
     'tienda',
     'carrito',
+    'ordenes',
+    'crispy_forms',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +139,20 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Carrito
 CARRITO_SESSION_ID = 'carrito'
+
+#Crispy Forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# django allauth
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_LOGOUT_REDIRECT_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
--- mi_negocio_web/urls.py
+++ mi_negocio_web/urls.py
@@ -21,6 +21,8 @@ urlpatterns = [
     path('', include('nucleo.urls', namespace='nucleo')),
     path('tienda/', include('tienda.urls', namespace='tienda')),
     path('carrito/', include('carrito.urls', namespace='carrito')),
+    path('ordenes/', include('ordenes.urls', namespace='ordenes')),
+    path('accounts/', include('allauth.urls')),
 ]
 
 from django.conf import settings

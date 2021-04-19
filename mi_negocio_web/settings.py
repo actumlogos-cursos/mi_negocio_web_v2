ff594f048ec7f903f595d870d01f2a71404b9394
Jesus <jmvazqueznicolas@gmail.com>
2021-04-19 15:24:00 -0500

    modificando

--- mi_negocio_web/settings.py
+++ mi_negocio_web/settings.py
@@ -137,6 +137,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'mi_negocio_web/static')
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
